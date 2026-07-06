b12c3e1ca1d946b526e61ca064fa9c933eec3557
Lois A Johnson <loisajohnson2007@gmail.com>
2026-07-06 20:54:49 +0530

    Update authRoutes.js

--- VLED-backend/routes/authRoutes.js
+++ VLED-backend/routes/authRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const faqController = require('../controllers/faqController');
 
+// Map endpoints to controller methods
+router.post('/signup', faqController.signup);
+router.post('/login', faqController.login);
+
+module.exports = router;
