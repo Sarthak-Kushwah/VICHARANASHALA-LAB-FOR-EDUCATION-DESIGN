f5188ca18c9005712c4b050b7364e48719c2d012
Lois A Johnson <loisajohnson2007@gmail.com>
2026-07-06 20:55:42 +0530

    Update faqRoutes.js

--- VLED-backend/routes/faqRoutes.js
+++ VLED-backend/routes/faqRoutes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const faqController = require('../controllers/faqController');
 
+// Saare routes Controller se link ho gaye
+router.get('/', faqController.getAllFaqs);
+router.post('/add', faqController.addFaq);
+router.get('/trending', faqController.getTrendingFaqs);
+router.get('/recent', faqController.getRecentFaqs);
+router.get('/category/:categoryName', faqController.getCategoryFaqs);
+router.get('/:id', faqController.increaseViews);
+router.post('/chat', faqController.chatDummy);
+
+module.exports = router;
