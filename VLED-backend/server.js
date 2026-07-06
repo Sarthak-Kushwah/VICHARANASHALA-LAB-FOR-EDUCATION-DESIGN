ba949d64719eb977cb032fb5fa72adbafc48edf6
Neeraj Agrawal <neerajagrawal689@gmail.com>
2026-07-06 21:08:37 +0530

    Update server.js

--- VLED-backend/server.js
+++ VLED-backend/server.js
@@ -1,23 +1,3 @@
-/*const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
-const faqRoutes = require('./routes/faqRoutes');
-
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// API Routes
-app.use('/api/faqs', faqRoutes);
-
-// Server Start
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT} with Turso DB!`);
-});*/
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
