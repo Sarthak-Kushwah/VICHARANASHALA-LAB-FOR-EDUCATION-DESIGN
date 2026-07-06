76b717ef1a7e16604af892b20ba27fd660235817
Akash Gaurav <akashgaurav347@gmail.com>
2026-07-06 21:15:34 +0530

    Update Faq.js

--- VLED-backend/models/Faq.js
+++ VLED-backend/models/Faq.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
 
+const faqSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true // e.g., "VLE Platform", "Team Formation"
+    },
+    views: {
+        type: Number,
+        default: 0 // Most popular nikalne ke liye kaam aayega
+    },
+    status: {
+        type: String,
+        default: 'Solved'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Faq', faqSchema);
